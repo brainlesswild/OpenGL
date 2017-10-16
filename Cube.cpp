d79cd1d466acdcaaab7e7bb621834efa08bed07f
Alexey Prigarin <alexey2304alexey@gmail.com>
2017-10-16 21:19:37 +0300

    Debug Version

--- Cube.cpp
+++ Cube.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glm/glm.hpp>
+#include <glm/transform.hpp>
 #include <unistd.h>
 #include <math.h>
 
@@ -19,35 +20,31 @@ class Treangle
 
 
 Treangle::Treangle(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3) :
-vertex_1(v1), vertex_2(v2), vertex_3(v3)
+vertex_1(v1), vertex_2(v2), vertex_3(v3);
 { }
 
 
 // Пармаетры экрана 
-int width = 800;
-int height = 600;
-int a = 20;
-int near_z = 1;
-int far_z = 7;
-int devider = width/height;    
-
-//Расстояние d
-float d = 0;
-
+const int width = 800;
+const int height = 600;
+const int near_z = 1;
+const int far_z = 7;
+const int devider = width/height;  
+float angle = 60.0f;
 
 class Cube
 {
     public:
         
-        void draw(glm::vec3 position);
-        void rotateX(float angle);
-        void rotateZ(float angle);
-        void virtualize(a,near_z,far_z);
-        
+        void draw(glm::vec position);
+    
     private:
         
-        glm::mat3 modelMatrix = glm::mat3(1.0); 
-    
+        glm::mat4 perspectMatrix = glm::mat4(1.0);
+        glm::mat4 rotateMatrix   = glm::mat4(1.0);
+        glm::mat4 posMatrix      = glm::mat4(1.0);
+        
+        glm::mat4 modelMatrix  = glm::mat4(1.0); // главная матрица !!!111
         
         const int number_treangles = 12;
         Treangle cube_tr[12] = {Treangle(glm::vec3(-0.5, -0.5, -0.5), glm::vec3(0.5, 0.5, -0.5), glm::vec3(0.5, -0.5, -0.5)),
@@ -72,47 +69,41 @@ class Cube
                                 
                                 Treangle(glm::vec3(-0.5, -0.5, -0.5), glm::vec3(0.5, -0.5, 0.5), glm::vec3(0.5, -0.5, -0.5)),
                                 Treangle(glm::vec3(-0.5, -0.5, -0.5), glm::vec3(0.5, -0.5, 0.5), glm::vec3(-0.5, -0.5, 0.5)) 
-                                };
+                                }; //VOVA TI DOWN KVADRAT
+                                
+        void CalculateModelMatrix();
+        void calculatePerspective();
+        void rotateX(float angle);
                             
 };
 
+void Cube::calculatePerspective()
+{
+    perspectMatrix = glm::perspective(angle, devider, near_z, far_z);
+}
+    
+
 void Cube::rotateX(float angle)
 {
-    glm::mat3 changeMatrix = glm::mat3(1.0,     0.0,        0.0,
-                                       0,   cos(angle), -sin(angle), 
-                                       0,   sin(angle),  cos(angle));  
-    modelMatrix = modelMatrix * changeMatrix;
+    glm::mat4 changeMatrix = glm::mat4(1.0, 0.0,                  0.0, 0.0
+                                       0.0,   cos(angle), -sin(angle), 0.0,
+                                       0.0,   sin(angle),  cos(angle), 0.0,
+                                       0.0,         0.0,   0.0,        1.0);  
+    rotateMatrix = rotateMatrix * changeMatrix;
 }
 
-void void virtualize(a,near_z,far_z)
+
+void Cube::CalculateModelMatrix()
 {
-    glm::mat3 virtual_mat = glm::mat3(1/(devider*tan(a/2)),      0.0, 0.0, 0.0,
-                                      0.0,         1/tan(a/2),        0.0, 0.0,
-                                      0.0,         0.0,  (-near_z - far_z)/(near_z-far_z), 2*near_z*far_z/near_z-far_z,
-                                      0.0,         0.0,          1.0,      0.0)
-    modelMatrix = modelMatrix * virtual_mat;
+    calculatePerspective();
+    modelMatrix = rotateMatrix * posMatrix * perspectMatrix;
 }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
-void Cube::rotateZ(float angle)
-{
-    glm::mat3 changeMatrix = glm::mat3(cos(angle), -sin(angle), 0.0,
-                                       sin(angle),  cos(angle), 0.0,
-                                       0.0,         0.0,        1.0);  
-    modelMatrix = modelMatrix * changeMatrix;
-}
 void Cube::draw(glm::vec3 position)
 {
+    CalculateModelMatrix();
+    
     for(int i = 0; i < number_treangles; i++)
     {
         
@@ -125,9 +116,10 @@ void Cube::draw(glm::vec3 position)
         glBegin(GL_TRIANGLES);
         glColor3f(0.1 + 0.04*i, 0.2 + 0.02*i, 0.04*i);
         
-        glVertex3f(changed_vertex_1.x, changed_vertex_1.y, changed_vertex_1.z);
-        glVertex3f(changed_vertex_2.x, changed_vertex_2.y, changed_vertex_2.z);
-        glVertex3f(changed_vertex_3.x, changed_vertex_3.y, changed_vertex_3.z);
+        for(int i = 0; i < 12; i++)
+        { 
+            glVertex3f(glm::vec3( modelMatrix * glm::vec4(cube_tr[i].vertex_1, 1)), glm::vec3( modelMatrix * glm::vec4(cube_tr[i].vertex_2, 1)), glm::vec3(modelMatrix * glm::vec4(cube_tr[i].vertex_3, 1)));
+        }
         
         glEnd();
     }
@@ -153,7 +145,6 @@ int main ()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         my_cube.draw(glm::vec3(x, y, 0));
         my_cube.rotateX(0.01);
-        
         window.display();
         
     }
